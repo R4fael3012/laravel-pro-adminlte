8f2d087b4498ce1a60215f64f9345adcdeadf098
EBSERH - Rafael Alves Rodrigues <rafael.rodrigues.3@ebserh.gov.br>
2026-04-29 17:12:38 -0300

    aula 48

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -246,7 +246,7 @@
             <li class="nav-item dropdown user-menu">
               <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                 <img
-                  src="./assets/img/user2-160x160.jpg"
+                  src="{{ asset('resources/img/user2-160x160.jpg') }}"
                   class="user-image rounded-circle shadow"
                   alt="User Image"
                 />
@@ -256,7 +256,7 @@
                 <!--begin::User Image-->
                 <li class="user-header text-bg-primary">
                   <img
-                    src="./assets/img/user2-160x160.jpg"
+                    src="{{ asset('resources/img/user2-160x160.jpg') }}"
                     class="rounded-circle shadow"
                     alt="User Image"
                   />
@@ -1454,7 +1454,7 @@
       /* new jsVectorMap({
         selector: '#world-map',
         map: 'world',
-      }); --> */
+      }); --> 
 
       <!-- // Sparkline charts
       /* const option_sparkline1 = {
